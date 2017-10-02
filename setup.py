0dfb19efe3208121f825a6e0b7737f68dfcf8b84
Bjoern Griebenow <b.griebenow@web.de>
2017-10-02 23:20:41 +0200

    Add setup.py file for pip3 uploading

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='cycle-breaker',
+      version='0.3.1',
+      description='Find cyclic references in source code',
+      url='http://github.com/schanur/cycle-breaker',
+      author='Björn Griebenow',
+      author_email='b.griebeno0w@web.de',
+      license='GPLv3',
+      packages=['cycle-breaker'],
+      zip_safe=False)
